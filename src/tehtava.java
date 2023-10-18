7479032f0c9e99550a0cb4b809c22042e89384a7
HetaEkb <heta23005@student.hamk.fi>
2023-10-18 19:18:16 +0300

    1.

--- src/tehtava.java
+++ src/tehtava.java
@@ -2,8 +2,30 @@
 public class tehtava {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+int ika = 20;
 
+		
+
+		if (ika >= 0 && ika < 18)
+
+		{
+
+		System.out.println("Olet alaikäinen");
+
+		}
+
+		else
+
+		{
+
+		System.out.println("Olet aikuinen");
+
+		}
+
+		if (ika > 0 && ika < 18) 
+			{ 
+			System.out.println("Olet alaikäinen"); 
+			} 
 	}
 
 }
